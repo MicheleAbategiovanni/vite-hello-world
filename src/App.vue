503c32aa3e7da28e850db10a10d7fbac43f5d180
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-01 14:00:43 +0100

    project initialization with sentence

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+
+  components: { AppTitle },
+
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<template>
+
+  <div>
+    <h1>
+      <AppTitle></AppTitle>
+    </h1>
+  </div>
+
+</template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+
+    export default{
+        data(){
+            return{
+                message: "La mia prima app con Vite!",
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<template>
+
+{{message}}
+
+</template>
+
